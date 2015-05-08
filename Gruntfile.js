91dbb1c878b209e772fa75fb4c055a32f12e9931
Andrew Stuart <andrew.stuart2@gmail.com>
2015-05-08 09:27:51 -0700

    Add docs to build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,7 @@ module.exports = function(grunt) {
         'concat',
         'ngAnnotate',
         'uglify',
+        'docs'
     ]);
 
     grunt.registerTask('default', 'build');
--- build/ng-drag.js
+++ build/ng-drag.js
@@ -1,71 +1,108 @@
 angular.module('ngDrag', []);
 
 angular.module('ngDrag').directive('ngDrag', ["DragData", function (DragData) {
-    return {
-        restrict: 'A',
-        link: function postLink($scope, element, iAttrs) {
-            element.attr('draggable', 'true');
+  /**
+   * @ngdoc directive
+   * @name ngDrag.directive:ngDrag
+   * @description A directive that allows an item to be dragged and its $scope
+   * tracked and provided to the drop target upon drop.
+   * @param {String} ngDrag Either emtpy or a string that can be used to
+   * identify the type of data that is being transferred. This is most useful
+   * for limiting where an item can be dropped. For example, say you show
+   * multiple items each with multiple steps, and you want to limit dragging
+   * steps to the same item.
+   *
+   * ```html
+   * <div class="item" ng-drag="item/{{item.id}}">...</div>
+   * ```
+   *
+   * @param {Expression} dragStart An expression that will be executed on
+   * dragstart. Event available as `$event`.
+   * @param {Expression} dragEnd An expression that will be evaluated on
+   * dragend. Event available as `$event`.
+   * @restrict A
+   */
+  return {
+    restrict: 'A',
+    link: function postLink($scope, element, iAttrs) {
+      element.attr('draggable', 'true');
 
-            if (iAttrs.dragStart) {
-                element.on('dragend', function(e) {
-                    $scope.$eval(iAttrs.dragStart, {$event: e});
-                    $scope.$apply();
-                });
-            }
+      if (iAttrs.dragEnd) {
+        iEle.on('dragend', function(e) {
+          $scope.$eval(iAttrs.dragStart, {$event: e});
+          $scope.$apply();
+        });
+      }
 
-            element.on('dragstart', function(e) {
-                e.stopPropagation();
-                e.originalEvent.dataTransfer.setData('ngdrag/type', iAttrs.ngDrag || 'ngdrag/id');
-                e.originalEvent.dataTransfer.setData(iAttrs.ngDrag || 'ngdrag/id', $scope.$id);
-                DragData.add($scope);
-            });
+      element.on('dragstart', function(e) {
+        e.stopPropagation();
+        e.originalEvent.dataTransfer.setData('ngdrag/type', iAttrs.ngDrag || 'ngdrag/id');
+        e.originalEvent.dataTransfer.setData(iAttrs.ngDrag || 'ngdrag/id', $scope.$id);
+        DragData.add($scope);
+
+        if (iAttrs.dragStart) {
+          $scope.$eval(iAttrs.dragStart, {$event: e});
+          $scope.$apply();
         }
-    };
+      });
+    }
+  };
 }]);
 
 angular.module('ngDrag').directive('ngDrop', ["DragData", function(DragData) {
 
-    /**
-     * @ngdoc directive
-     * @name uni.directive:ngDrop
-     * @param ngDrop The expression to evaluate upon drop. If dropped element came from ngDrag, the ngDrag $scope is
-     * available as $from.
-     * @description Allows an expression to be evaluated upon drop. Event available as $event.
-     * @restrict A
-     */
-    return {
-        restrict: 'A',
-        link: function postLink($scope, element, iAttrs) {
+  /**
+   * @ngdoc directive
+   * @name uni.directive:ngDrop
+   * @param {Expression} ngDrop The expression to evaluate upon drop. If dropped element came from ngDrag, the ngDrag $scope is
+   * available as $from.
+   * @param {Expression} dragOver An expression to evaluate on dragover.
+   * @param {String} allowDrop A string that identifies the type of data that
+   * will trigger the ngDrop handler. Used in conjuction with `ng-drag="..."`,
+   * this is helpful for limiting what can be dropped on a particular target.
+   * @description Allows an expression to be evaluated upon drop. Event
+   * available as $event.
+   * @restrict A
+   */
+  return {
+    restrict: 'A',
+    link: function postLink($scope, element, iAttrs) {
 
-            element.on('dragover', function(e) { e.preventDefault(); });
+      element.on('dragover', function(e) {
+        e.preventDefault();
 
-            element.on('dragenter', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                var type = e.originalEvent.dataTransfer.getData('ngdrag/type');
-                if(type === iAttrs.allowDrop) {
-                    event.dataTransfer.dropEffect = 'move';
-                } else {
-                    event.dataTransfer.dropEffect = 'none';
-                }
-            });
-
-            element.on('drop', function(e) {
-                e.preventDefault();
-
-                var id = e.originalEvent.dataTransfer.getData(iAttrs.allowDrop || 'ngdrag/id');
-                if(!id) { return; }
-                var from = DragData.get(id);
-
-                $scope.$apply(function() {
-                    $scope.$eval(iAttrs.ngDrop, {
-                        $from: from,
-                        $event: e
-                    });
-                });
-            });
+        if (iAttrs.dragOver) {
+          $scope.$eval(iAttrs.dragOver, {$event: e});
         }
-    };
+      });
+
+      element.on('dragenter', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var type = e.originalEvent.dataTransfer.getData('ngdrag/type');
+        if(type === iAttrs.allowDrop) {
+          event.dataTransfer.dropEffect = 'move';
+        } else {
+          event.dataTransfer.dropEffect = 'none';
+        }
+      });
+
+      element.on('drop', function(e) {
+        e.preventDefault();
+
+        var id = e.originalEvent.dataTransfer.getData(iAttrs.allowDrop || 'ngdrag/id');
+        if(!id) { return; }
+        var from = DragData.get(id);
+
+        $scope.$apply(function() {
+          $scope.$eval(iAttrs.ngDrop, {
+            $from: from,
+            $event: e
+          });
+        });
+      });
+    }
+  };
 }]);
 
 angular.module('ngDrag').service('DragData', function () {
@@ -73,12 +110,20 @@ angular.module('ngDrag').service('DragData', function () {
      * @ngdoc service
      * @name uni.service:DragData
      * @description
-     * # dragger service in the sabApp.  */
+     * The DragData service is a simple service for holding references to a
+     * scope by its id and cleaning up at the right time. It is used by ngDrag
+     * to get reference to the right scope on the drop event.
+     */
         var index = {};
 
     this.add = function(scope) {
         if(scope.$id) {
             index[scope.$id] = scope;
+
+            //Cleanup
+            scope.$on('$destroy', function() {
+              delete index[scope.$id];
+            });
         }
     };
 
