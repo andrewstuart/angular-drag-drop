c52430cda64d7ea148a50ba3069a8d8d877ac950
Andrew Stuart <andrew.stuart2@gmail.com>
2015-05-08 09:35:25 -0700

    Update build to include LICENSE

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,63 @@
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        concat: {
-            all: {
-                src: [
-                    'src/module.js',
-                    'src/*/**/*.js'
-                ],
-                dest: 'build/ng-drag.js'
-            }
+  grunt.initConfig({
+    license: '/*! <%= grunt.file.read("./LICENSE") %>*/\n\n',
+    concat: {
+      all: {
+        options: {
+          banner: '<%= license %>'
         },
-        uglify: {
-            drag: {
-                files: {
-                    'build/ng-drag.min.js': ['build/ng-drag.js']
-                }
-            }
-        },
-        ngAnnotate: {
-            survey: {
-                files: { 'build/ng-drag.js': ['build/ng-drag.js'] }
-            }
-        },
-        watch: {
-            all: {
-                files: ['src/**/*.js'],
-                tasks: [
-                  'build',
-                  'docs'
-                ],
-                options: { spawn: false }
-            }
-        },
-        ngdocs: {
-          options: {
-            dest: 'docs'
-          },
-          ngdrag: {
-            src: ['src/**/*.js'],
-            title: 'ngDrag'
-          }
+        src: [
+          'src/module.js',
+          'src/*/**/*.js'
+        ],
+        dest: 'build/ng-drag.js'
+      }
+    },
+    uglify: {
+      drag: {
+        files: {
+          'build/ng-drag.min.js': ['build/ng-drag.js']
         }
-    });
+      }
+    },
+    ngAnnotate: {
+      survey: {
+        files: { 'build/ng-drag.js': ['build/ng-drag.js'] }
+      }
+    },
+    watch: {
+      all: {
+        files: ['src/**/*.js'],
+        tasks: [
+          'build',
+          'docs'
+        ],
+        options: { spawn: false }
+      }
+    },
+    ngdocs: {
+      options: {
+        dest: 'docs'
+      },
+      ngdrag: {
+        src: ['src/**/*.js'],
+        title: 'ngDrag'
+      }
+    }
+  });
 
-    grunt.registerTask('build', [
-        'concat',
-        'ngAnnotate',
-        'uglify',
-        'docs'
-    ]);
+  grunt.registerTask('build', [
+    'concat',
+    'ngAnnotate',
+    'uglify',
+    'docs'
+  ]);
 
-    grunt.registerTask('default', 'build');
+  grunt.registerTask('default', 'build');
 
-    grunt.registerTask('docs', [
-      'ngdocs'
-    ]);
+  grunt.registerTask('docs', [
+    'ngdocs'
+  ]);
 };
