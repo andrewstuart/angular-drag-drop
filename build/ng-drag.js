bc6688c64f9551b0a8c60e928fcdbecbd7692088
Andrew Stuart <andrew.stuart2@gmail.com>
2015-08-13 09:27:31 -0700

    Update documentation and correct some dragleave issues

--- build/ng-drag.js
+++ build/ng-drag.js
@@ -86,7 +86,7 @@ angular.module('ngDrag')
 }]);
 
 angular.module('ngDrag')
-.directive('ngDragenter', function () {
+.directive('ngDragenter', ["$timeout", function ($timeout) {
   'use strict';
 
   /**
@@ -100,30 +100,39 @@ angular.module('ngDrag')
     restrict: 'A',
     link: function postLink($scope, iEle, iAttrs) {
       iEle.on('dragenter', function(e) {
-        $scope.$eval(iAttrs.ngDragenter, {$event: e});
+        $timeout(function() {
+          $scope.$eval(iAttrs.ngDragenter, {$event: e});
+        });
       });
     }
   };
-})
-.directive('ngDragleave', function () {
+}])
+.directive('ngDragleave', ["$timeout", function ($timeout) {
   'use strict';
 
   /**
    * @ngdoc directive
-   * @name ngDrag.directive:ngDragenter
-   * @description A directive that will execute some function on dragenter
-   * @param {Expression} ngDragenter An expression to be executed on dragenter.
+   * @name ngDrag.directive:ngDragleave
+   * @description A directive that will execute the provided expression on dragleave
+   * @param {Expression} ngDragleave An expression to be executed on dragenter.
    * @restrict A
    */
   return {
     restrict: 'A',
     link: function postLink($scope, iEle, iAttrs) {
       iEle.on('dragleave', function(e) {
-        $scope.$eval(iAttrs.ngDragleave, {$event: e});
+        //Prevent event from bubbling past the target.
+        if ( e.target === iEle[0] ) {
+          e.stopPropagation();
+        }
+
+        $timeout(function() {
+          $scope.$eval(iAttrs.ngDragleave, {$event: e});
+        });
       });
     }
   };
-});
+}]);
 
 angular.module('ngDrag')
 .directive('ngDrop', ["DragData", function(DragData) {
