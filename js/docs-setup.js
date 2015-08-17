3261bfc55b72f1f19a4065b5753b5be9a4026a9d
Andrew Stuart <andrew.stuart2@gmail.com>
2015-08-17 15:08:20 -0700

    Add documentation branch

---
+++ js/docs-setup.js
@@ -0,0 +1,61 @@
+NG_DOCS={
+  "sections": {
+    "pages": "ngDrag"
+  },
+  "pages": [
+    {
+      "section": "pages",
+      "id": "ngDrag.directive:ngDrag",
+      "shortName": "ngDrag",
+      "type": "directive",
+      "moduleName": "ngDrag",
+      "shortDescription": "A directive that allows an item to be dragged and its $scope",
+      "keywords": "$event $scope allows class data directive dragend dragged dragging dragstart drop dropped emtpy evaluated event example executed expression html identify item items limit limiting multiple ng-drag ngdrag ngdragend ngdragstart provided steps string target tracked transferred type"
+    },
+    {
+      "section": "pages",
+      "id": "ngDrag.directive:ngDragenter",
+      "shortName": "ngDragenter",
+      "type": "directive",
+      "moduleName": "ngDrag",
+      "shortDescription": "A directive that will execute some function on dragenter",
+      "keywords": "directive dragenter execute executed expression function ngdrag ngdragenter"
+    },
+    {
+      "section": "pages",
+      "id": "ngDrag.directive:ngDragleave",
+      "shortName": "ngDragleave",
+      "type": "directive",
+      "moduleName": "ngDrag",
+      "shortDescription": "A directive that will execute the provided expression on dragleave",
+      "keywords": "directive dragenter dragleave execute executed expression ngdrag ngdragleave provided"
+    },
+    {
+      "section": "pages",
+      "id": "uni.directive:ngDrop",
+      "shortName": "ngDrop",
+      "type": "directive",
+      "moduleName": "uni",
+      "shortDescription": "Allows an expression to be evaluated upon drop. Event",
+      "keywords": "$event $from $scope allowdrop allows called conjuction data directive dragover drop dropped element evaluate evaluated event expression function handler helpful identifies limiting ng-drag ng-drop ngdrag ngdragover ngdrop provided string target trigger type uni"
+    },
+    {
+      "section": "pages",
+      "id": "uni.service:DragData",
+      "shortName": "DragData",
+      "type": "service",
+      "moduleName": "uni",
+      "shortDescription": "The DragData service is a simple service for holding references to a",
+      "keywords": "cleaning dragdata drop event holding ngdrag reference references scope service simple time uni"
+    }
+  ],
+  "apis": {
+    "pages": false
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
