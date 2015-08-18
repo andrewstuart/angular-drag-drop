a1008ea56571ce8a37961972f51ee95cccf7b52c
Andrew Stuart <andrew.stuart2@gmail.com>
2015-08-17 17:34:00 -0700

    Update documentation with examples.

--- src/directives/ng-drag.js
+++ src/directives/ng-drag.js
@@ -23,19 +23,82 @@ angular.module('angular-drag-drop')
    * dragend. Event available as `$event`.
    * @restrict A
    * @example
-   * <example>
+   * <example module="test">
    *   <file name="example.js">
    *     angular.module('test', ['angular-drag-drop'])
-   *     .controller('example', function($scope) {
-   *       $scope.tests = ['foo', 'bar', 'baz'];
+   *     .controller('ExController', function($scope) {
+   *       $scope.tests = ['green', 'yellow', 'red'];
+   *       $scope.alert = function(msg) {
+   *         alert(msg);
+   *       };
    *     });
    *   </file>
    *   <file name="example.html">
-   *     <div ng-app="test" ng-controller="ExController">
-   *       <div ng-repeat="data in tests" ng-drag="test">{{data}}</div>
-   *       <div ng-drop="$scope.data = $from.data" ng-allow-drop="test">{{data}}</div>
+   *     <style>
+   *       .parent div {
+   *         height: 3em;
+   *         background-color: rgba(0, 0, 0, 0.1);
+   *         margin: 1em;
+   *       }
+   *     </style>
+   *     <div class="parent" ng-controller="ExController">
+   *       <div ng-repeat="data in tests" style="background-color: {{data}};" ng-drag>
+   *         {{data}} (Drag Me)
+   *       </div>
+   *       <div ng-drop="dropped = $from.data" style="background-color: {{dropped}};">
+   *         {{dropped}} (Drop{{dropped && 'ped'}} here)
+   *       </div>
    *     </div>
    *   </file>
+   * </example>
+   * <example module="test2">
+   * <file name="example2.js">
+   *   angular.module('test2', ['angular-drag-drop'])
+   *     .controller('test', function($scope) {
+   *       $scope.data = [{
+   *         color: 'red',
+   *         numbers: [1, 2, 3]
+   *       },{
+   *         color: 'green',
+   *         numbers: [4, 5, 6]
+   *       },{
+   *         color: 'blue',
+   *         numbers: [7, 8, 9]
+   *       }]
+   *     });
+   * </file>
+   * <file name="example2.html">
+   *   <style>
+   *     .target {
+   *       background-color: #ddd;
+   *       height: 2em;
+   *       min-width: 8em;
+   *     }
+   *
+   *
+   *     .num {
+   *       display: inline-block;
+   *       background-color: rgba(0, 0, 0, 0.1);
+   *       padding: 1em;
+   *       text-align: center;
+   *       color: white;
+   *       margin: 1em;
+   *     }
+   *
+   *     .target.num { color: black; }
+   *   </style>
+   *   <p>Notice you can only drag between like colors.</p>
+   *   <div ng-controller="test">
+   *     <div ng-repeat="color in data" style="background-color: {{color.color}};">
+   *       <div class="num" ng-repeat="number in color.numbers" ng-drag="color/{{color.color}}">
+   *         {{number}}
+   *       </div>
+   *       <div class="target num" ng-drop="current = $from.number" allow-drop="color/{{color.color}}">
+   *         {{current || 'Drag ' + color.color + ' Here'}}
+   *       </div>
+   *     </div>
+   *   </div>
+   * </file>
    * </example>
    */
   return {
